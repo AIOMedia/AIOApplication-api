cb32ce68d748c63324a3010d728d448de44ccd5c
Elorfin <penin.axel@gmail.com>
2015-01-04 23:02:02 +0100

    use new crud controller

--- app/Music/routes.js
+++ app/Music/routes.js
@@ -2,24 +2,24 @@ var express = require('express');
 var router = express.Router();
 
 // Load Artist Controller to have access to CRUD methods
-var artistCtrl = require('./Controllers/ArtistController');
+/*var artistCtrl = require('./Controllers/ArtistController');*/
 
-var crudCtrl = require('../Core/Controllers/CRUDController');
-var artistCtrl = new crudCtrl('Music/Models/Artist');
+var CRUDController = require('../Core/Controllers/CRUDController');
+var artistCtrl = new CRUDController('Music/Models/Artist');
 
 /* LIST */
-router.get('/artist/',       artistCtrl.list);
+router.get('/artist/',       artistCtrl.list.bind(artistCtrl));
 
 /* GET */
-router.get('/artist/:id',    artistCtrl.get);
+router.get('/artist/:id',    artistCtrl.get.bind(artistCtrl));
 
 /* CREATE */
-router.post('/artist/',      artistCtrl.create);
+router.post('/artist/',      artistCtrl.create.bind(artistCtrl));
 
 /* UPDATE */
-router.put('/artist/:id',    artistCtrl.update);
+router.put('/artist/:id',    artistCtrl.update.bind(artistCtrl));
 
 /* DELETE */
-router.delete('/artist/:id', artistCtrl.delete);
+router.delete('/artist/:id', artistCtrl.delete.bind(artistCtrl));
 
 module.exports = router;
