623d6db9735ea127f0b2701e6a3706a9c24b09e6
Elorfin <penin.axel@gmail.com>
2015-01-05 08:19:35 +0100

    some tries to handle file upload

--- app/File/routes.js
+++ app/File/routes.js
@@ -1,23 +1,47 @@
 var express = require('express');
 var router = express.Router();
 
+var path = require('path');
+var fs   = require('fs');
+
+var multipart = require('connect-multiparty');
+var multipartMiddleware = multipart();
+
 // Load Artist Controller to have access to CRUD methods
 var CRUDController = require('../Core/Controllers/CRUDController');
 var fileCtrl = new CRUDController('File/Models/File');
 
+fileCtrl.upload = function (req, res) {
+    var tempPath = req.files.file.path;
+    var targetPath = path.resolve('./upload/' + req.files.file.name);
+
+    fs.rename(tempPath, targetPath, function(err) {
+        if (err) {
+            throw err;
+        }
+
+        res.writeHead(200, {'Content-Type': req.files.file.type });
+        res.setEncoding('binary');
+        // stream the file
+        fs.createReadStream(targetPath, 'utf-8').pipe(res);
+    });
+};
+
 /* LIST */
-router.get('/artist/',       fileCtrl.list);
+router.get('/',       fileCtrl.list.bind(fileCtrl));
 
 /* GET */
-router.get('/artist/:id',    fileCtrl.get);
+router.get('/:id',    fileCtrl.get.bind(fileCtrl));
 
 /* CREATE */
-router.post('/artist/',      fileCtrl.create);
+router.post('/',      fileCtrl.create.bind(fileCtrl));
+
+router.post('/upload', multipartMiddleware, fileCtrl.upload.bind(fileCtrl));
 
 /* UPDATE */
-router.put('/artist/:id',    fileCtrl.update);
+router.put('/:id',    fileCtrl.update.bind(fileCtrl));
 
 /* DELETE */
-router.delete('/artist/:id', fileCtrl.delete);
+router.delete('/:id', fileCtrl.delete.bind(fileCtrl));
 
 module.exports = router;
